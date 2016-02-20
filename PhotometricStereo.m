7841b0f6286cdfa2568c3405e5d217152cd755ea
jhartman <jhartman@sfu.ca>
2016-02-19 22:18:43 -0800

    Add: Use ginput to find and gather information about the calaboration sphere.
    Plot cicle on image.

--- PhotometricStereo.m
+++ PhotometricStereo.m
@@ -7,5 +7,16 @@ imgSphere = imread('Photostereo_RealImages/sphere-lamp1.tif');
 %Convert to gray
 imgSphereGray = rgb2gray(imgSphere);
 imshow(imgSphereGray)
-[x,y] = ginput(1)
+[cx, cy] = ginput(1)
+[px, py] = ginput(1)
 
+% Find the radius of circle
+radius = sqrt((cx - px)^2 + (cy - py)^2);
+
+% Draw circle on image
+theta = 0 : (2 * pi /10000) : (2 * pi);
+pline_x = radius * cos(theta) + cx;
+pline_y = radius * sin(theta) + cy;
+hold on; 
+plot(pline_x, pline_y)
+hold off;
