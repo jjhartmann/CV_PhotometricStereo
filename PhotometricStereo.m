428163a9c2299b7c57e77a0cdef35e00320d92e0
jhartman <jhartman@sfu.ca>
2016-02-19 21:57:31 -0800

    Add image sphere and convert to gray scale.

--- PhotometricStereo.m
+++ PhotometricStereo.m
@@ -1,3 +1,11 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Assingment 2 - Photostereo Imaging
 
+%% Init calabration Sphere. 
+imgSphere = imread('Photostereo_RealImages/sphere-lamp1.tif');
+
+%Convert to gray
+imgSphereGray = rgb2gray(imgSphere);
+imshow(imgSphereGray)
+[x,y] = ginput(1)
+
