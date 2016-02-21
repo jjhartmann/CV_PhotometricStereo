6f05489d34cff2554420177d5439586fa9fb7b3d
jhartman <jhartman@sfu.ca>
2016-02-20 20:28:01 -0800

    Load image data for 3D mapping.

--- PhotometricStereo_BuildObject.m
+++ PhotometricStereo_BuildObject.m
@@ -1,3 +1,14 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Assingment 2 - Photostereo Imaging: Building 3D Objects. 
 
+% Load TableLookUp
+load('LookUpTable.mat');
+
+% Load image data
+cylinderdata = {'Photostereo_RealImages/cylinder-lamp1.tif'; 'Photostereo_RealImages/cylinder-lamp2.tif'; 'Photostereo_RealImages/cylinder-lamp3.tif'};
+
+img1 = rgb2gray(imread(cylinderdata{1}));
+img2 = rgb2gray(imread(cylinderdata{2}));
+img3 = rgb2gray(imread(cylinderdata{3}));
+
+
